b846a98301f29741736fe2bdfcfff7513e487487
Will Fisher <will@gina.alaska.edu>
2015-05-10 19:03:28 -0800

    adding CORS

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Stories
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Alaska'
-    
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -28,5 +28,16 @@ module Stories
     config.active_record.raise_in_transactional_callbacks = true
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins /(teknofire\.net|xip.io)/
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 3628800
+      end
+    end
   end
 end
