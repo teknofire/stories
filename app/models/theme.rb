53fa4931d420b7cb1fc4934190c69211079fc1c3
Will Fisher <will@gina.alaska.edu>
2015-06-08 17:25:19 -0800

    another attempt at fixing theme name display

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,9 @@
 class Theme < ActiveRecord::Base
   validate :only_one_site
-
+ 
+  def to_s
+    title.blank? ? id : title
+  end
 
   def only_one_site
     if self.site? and Theme.where(site: true).where.not(id: self.id).count > 0
