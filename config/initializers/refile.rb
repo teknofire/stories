36d74f28d8664d0d9f7428ea9b25b187e32ba8cb
Will Fisher <will@gina.alaska.edu>
2016-02-14 11:57:23 -0900

    update initializer for refile-s3 gem

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
     bucket: "s2-stories",
     region: 'us-west-2'
   }
-  Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
-  Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::S3.new(prefix: "store", **aws)
   Refile.host = "//cdn.torquaret.teknofire.net"
 end
