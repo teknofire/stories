7e1702352887db5e32d26a676b94da8626d19d02
Will Fisher <will@gina.alaska.edu>
2016-02-14 21:23:47 -0900

    adding configs for redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+if Rails.application.secrets.redis_url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: Rails.application.secrets.redis_url, network_timeout: 5 }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: Rails.application.secrets.redis_url, network_timeout: 5 }
+  end
+end
+"redis://#{Rails.application.secrets.redis_host}:6379/12"
